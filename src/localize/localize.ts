dd9edc3acd296c8226f3c3b183f988fab84666d0
MiisaTrAnCe <83216950+MiisaTrAnCe@users.noreply.github.com>
2023-05-17 12:16:17 +0200

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -2,6 +2,7 @@ import * as en from './languages/en.json';
 import * as fr from './languages/fr.json';
 import * as pt from './languages/pt.json';
 import * as sl from './languages/sl.json';
+import * as cs from './languages/cs.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
@@ -9,6 +10,7 @@ const languages: any = {
   fr: fr,
   pt: pt,
   sl: sl,
+  cs: cs,
 };
 
 export const CARD_LANGUAGES = [...Object.keys(languages), ''].sort();
